fab9e653e9c7a8d7cea0498f1a9092727c19f135
Mattias Gustafsson <mattias.lanzen@gmail.com>
2015-09-25 13:47:07 +0200

    First test

---
+++ MainForm.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace ConventionOfElements
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.buttonStart = new System.Windows.Forms.Button();
+			this.timerPerSecond = new System.Windows.Forms.Timer(this.components);
+			this.labelInfoText = new System.Windows.Forms.Label();
+			this.buttonExit = new System.Windows.Forms.Button();
+			this.labelCounter = new System.Windows.Forms.Label();
+			this.comboBoxBuffs = new System.Windows.Forms.ComboBox();
+			this.labelBuffText = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// buttonStart
+			// 
+			this.buttonStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.buttonStart.Location = new System.Drawing.Point(12, 231);
+			this.buttonStart.Name = "buttonStart";
+			this.buttonStart.Size = new System.Drawing.Size(75, 23);
+			this.buttonStart.TabIndex = 0;
+			this.buttonStart.Text = "Start";
+			this.buttonStart.UseVisualStyleBackColor = true;
+			this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+			// 
+			// timerPerSecond
+			// 
+			this.timerPerSecond.Interval = 1000;
+			this.timerPerSecond.Tick += new System.EventHandler(this.timerPerSecond_Tick);
+			// 
+			// labelInfoText
+			// 
+			this.labelInfoText.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.labelInfoText.Font = new System.Drawing.Font("BankGothic Md BT", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.labelInfoText.ForeColor = System.Drawing.Color.White;
+			this.labelInfoText.Location = new System.Drawing.Point(12, 9);
+			this.labelInfoText.Name = "labelInfoText";
+			this.labelInfoText.Size = new System.Drawing.Size(310, 105);
+			this.labelInfoText.TabIndex = 1;
+			this.labelInfoText.Text = "label1";
+			this.labelInfoText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.labelInfoText.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MovingMouseDown);
+			// 
+			// buttonExit
+			// 
+			this.buttonExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.buttonExit.Location = new System.Drawing.Point(247, 231);
+			this.buttonExit.Name = "buttonExit";
+			this.buttonExit.Size = new System.Drawing.Size(75, 23);
+			this.buttonExit.TabIndex = 2;
+			this.buttonExit.Text = "Exit";
+			this.buttonExit.UseVisualStyleBackColor = true;
+			this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+			// 
+			// labelCounter
+			// 
+			this.labelCounter.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.labelCounter.Font = new System.Drawing.Font("BankGothic Md BT", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.labelCounter.ForeColor = System.Drawing.Color.White;
+			this.labelCounter.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+			this.labelCounter.Location = new System.Drawing.Point(12, 173);
+			this.labelCounter.Name = "labelCounter";
+			this.labelCounter.Size = new System.Drawing.Size(310, 35);
+			this.labelCounter.TabIndex = 3;
+			this.labelCounter.Text = "4";
+			this.labelCounter.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			this.labelCounter.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MovingMouseDown);
+			// 
+			// comboBoxBuffs
+			// 
+			this.comboBoxBuffs.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+			this.comboBoxBuffs.FormattingEnabled = true;
+			this.comboBoxBuffs.Items.AddRange(new object[] {
+            "Arcane",
+            "Cold",
+            "Fire",
+            "Holy",
+            "Lightning",
+            "Physical",
+            "Poison"});
+			this.comboBoxBuffs.Location = new System.Drawing.Point(116, 233);
+			this.comboBoxBuffs.Name = "comboBoxBuffs";
+			this.comboBoxBuffs.Size = new System.Drawing.Size(105, 21);
+			this.comboBoxBuffs.TabIndex = 4;
+			this.comboBoxBuffs.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+			// 
+			// labelBuffText
+			// 
+			this.labelBuffText.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.labelBuffText.Font = new System.Drawing.Font("BankGothic Md BT", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.labelBuffText.ForeColor = System.Drawing.Color.White;
+			this.labelBuffText.Location = new System.Drawing.Point(12, 114);
+			this.labelBuffText.Name = "labelBuffText";
+			this.labelBuffText.Size = new System.Drawing.Size(310, 59);
+			this.labelBuffText.TabIndex = 5;
+			this.labelBuffText.Text = "label3";
+			this.labelBuffText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.labelBuffText.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MovingMouseDown);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.Black;
+			this.ClientSize = new System.Drawing.Size(334, 271);
+			this.Controls.Add(this.labelBuffText);
+			this.Controls.Add(this.comboBoxBuffs);
+			this.Controls.Add(this.labelCounter);
+			this.Controls.Add(this.buttonExit);
+			this.Controls.Add(this.labelInfoText);
+			this.Controls.Add(this.buttonStart);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "MainForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Convention Of Elements - Helper";
+			this.Load += new System.EventHandler(this.MainForm_Load);
+			this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MovingMouseDown);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button buttonStart;
+		private System.Windows.Forms.Timer timerPerSecond;
+		private System.Windows.Forms.Label labelInfoText;
+		private System.Windows.Forms.Button buttonExit;
+		private System.Windows.Forms.Label labelCounter;
+		private System.Windows.Forms.ComboBox comboBoxBuffs;
+		private System.Windows.Forms.Label labelBuffText;
+	}
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConventionOfElements
+{
+	public partial class MainForm : Form
+	{
+		public const int WM_NCLBUTTONDOWN = 0xA1;
+		public const int HT_CAPTION = 0x2;
+
+		[DllImport("user32.dll")]
+		public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+		[DllImport("user32.dll")]
+		public static extern bool ReleaseCapture();
+
+		private void MovingMouseDown(object sender, MouseEventArgs e)
+		{
+			if (e.Button != MouseButtons.Left) return;
+			ReleaseCapture();
+			SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+		}
+
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		private readonly List<string> _buffs = new List<string>();
+		private int _buff = 0;
+		private int _alert = -1;
+		private int _count = 4;
+
+		private void buttonStart_Click(object sender, EventArgs e)
+		{
+			if (_alert == -1)
+			{
+				MessageBox.Show("Please select from dropdown what buff you want to track!");
+				return;
+			}
+			if (timerPerSecond.Enabled)
+			{
+				timerPerSecond.Stop();
+				buttonStart.Text = "Start";
+			}
+			else
+			{
+				timerPerSecond.Start();
+				_buff = 0;
+				buttonStart.Text = "Pause";
+				labelCounter.Text = _count.ToString();
+				labelBuffText.Text = _buffs[_buff];
+				labelBuffText.ForeColor = GetColorByNumber(_buff);
+				if (_buff == _alert)
+				{
+					labelInfoText.Text = "ACTIVE\r\nBUFF";
+					this.BackColor = Color.Crimson;
+					labelInfoText.ForeColor = Color.Black;
+				}
+				else
+				{
+					labelInfoText.Text = "inactive\r\nbuff";
+					this.BackColor = Color.Black;
+					labelInfoText.ForeColor = Color.White;
+				}
+			}
+		}
+
+		private void timerPerSecond_Tick(object sender, EventArgs e)
+		{
+			_count--;
+			if (_count != 0)
+			{
+				labelCounter.Text = _count.ToString();
+				if (_buff == _alert)
+					System.Media.SystemSounds.Exclamation.Play();
+				return;
+			}
+			_count = 4;
+			labelCounter.Text = _count.ToString();
+			_buff++;
+			if (_buff == _alert)
+			{
+				//System.Media.SystemSounds.Beep.Play();
+				//System.Media.SystemSounds.Asterisk.Play();
+				System.Media.SystemSounds.Exclamation.Play();
+				//System.Media.SystemSounds.Question.Play();
+				//System.Media.SystemSounds.Hand.Play();
+				labelInfoText.Text = "ACTIVE\r\nBUFF";
+				this.BackColor = Color.Crimson;
+				labelInfoText.ForeColor = Color.Black;
+			}
+			else
+			{
+				labelInfoText.Text = "inactive\r\nbuff";
+				this.BackColor = Color.Black;
+				labelInfoText.ForeColor = Color.White;
+			}
+	
+			labelBuffText.Text = _buffs[_buff];
+			labelBuffText.ForeColor = GetColorByNumber(_buff);
+
+			if (_buff >= 6)
+				_buff = 0;
+		}
+
+		private static Color GetColorByNumber(int buff)
+		{
+			switch (buff)
+			{
+				case 0:
+					return Color.Purple;
+				case 1:
+					return Color.Blue;
+				case 2:
+					return Color.DarkRed;
+				case 3:
+					return Color.Yellow;
+				case 4:
+					return Color.White;
+				case 5:
+					return Color.Gray;
+				case 6:
+					return Color.Green;
+				default:
+					return Color.White;
+			}
+		}
+
+		private void buttonExit_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+
+		private void MainForm_Load(object sender, EventArgs e)
+		{
+			labelInfoText.Text = "";
+			labelCounter.Text = "";
+			labelBuffText.Text = "";
+
+			_buffs.Add("Arcane");
+			_buffs.Add("Cold");
+			_buffs.Add("Fire");
+			_buffs.Add("Holy");
+			_buffs.Add("Lightning");
+			_buffs.Add("Physical");
+			_buffs.Add("Poison");
+		}
+
+		private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			var s = (ComboBox)sender;
+			for (int i = 0; i < _buffs.Count; i++)
+			{
+				if (_buffs[i] == s.Text)
+					_alert = i;
+			}
+		}
+
+
+	}
+}
+
